b41e4969ac1bef83feb504a43741c5180cd2aa7a
Florian Appel <florian.appel1@gmail.com>
2022-09-16 17:05:50 +0200

    updates

--- django_chat_app/settings.py
+++ django_chat_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^4d$8%*ac8sy5vr*&30jsezb@f484q-q*c)zvqdow7aam64bwr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['appelflorian1.pythonanywhere.com']
 
 
 # Application definition
